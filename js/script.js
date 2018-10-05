e2fb223b0ddd70ce7e4fc7eaf0215bb74e0a1712
Roman Krivenko <roman-kr-2017@ukr.net>
2018-10-05 22:47:51 +0300

    add section content description

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ function onChangeSocialShowButton() {
         socialShowList.style.display = "none";
     }
 }
+
+function onChangeHeaderDescription() {
+    let description = document.querySelector('.section.content .list .description');
+    let items = description.children;
+    for (let i = 0; i < items.length; i++) {
+        let isChecked = items[i].querySelector('input').checked;
+        if (isChecked) {
+            items[i].classList.add('active');
+        } else {
+            items[i].classList.remove('active');
+        }
+    }
+}
