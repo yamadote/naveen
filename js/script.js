937431dfe717f5bf18aa899d7ed037bd5f5041f4
Roman Krivenko <roman-kr-2017@ukr.net>
2018-10-07 19:26:32 +0300

    add animation to description

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-function onChangeHeaderDescription() {
-    let description = document.querySelector('.section.content .list .description');
-    let items = description.children;
-    for (let i = 0; i < items.length; i++) {
-        let isChecked = items[i].querySelector('input').checked;
-        if (isChecked) {
-            items[i].classList.add('active');
-        } else {
-            items[i].classList.remove('active');
-        }
-    }
-}
-
 function slider(id, slide) {
     let slider = document.getElementById(id);
     let slides = slider.getElementsByClassName('slides')[0];
